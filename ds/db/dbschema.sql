554fd6ff5349ab199824ff4cce422e4d4803662f
Ubuntu <ubuntu@ip-172-31-29-132.ap-northeast-2.compute.internal>
2019-05-21 01:18:27 +0000

    change dbschema

--- ds/db/dbschema.sql
+++ ds/db/dbschema.sql
@@ -7,7 +7,7 @@ ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
 
 CREATE DATABASE ds DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-
+use ds;
 CREATE TABLE data(
 	idx int(1) not null AUTO_INCREMENT,
 	data varchar(20),
